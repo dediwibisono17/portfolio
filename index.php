5e112987d106326bfdbfd7f074101ade95792fff
Dedi Arief Wibisono <dedi.wibisono17@gmail.com>
2016-05-02 15:47:37 +0700

    upload

---
+++ index.php
@@ -0,0 +1,331 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Dedi's Portfolio</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="css/main.css" rel="stylesheet" media="screen">
+    <link href="font-awesome-4.4.0/css/font-awesome.min.css" rel="stylesheet" />
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  
+  </head>
+  <body>
+
+   <!--  <nav class="navbar navbar-default" role="navigation">
+     <div class="container-fluid">
+       Brand and toggle get grouped for better mobile display
+       <div class="navbar-header">
+         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#nav-atas">
+           <span class="sr-only">Toggle navigation</span>
+           <span class="icon-bar"></span>
+           <span class="icon-bar"></span>
+           <span class="icon-bar"></span>
+         </button>
+         <a class="navbar-brand" href="#">LinkedIn</a>
+       </div>
+   
+       Collect the nav links, forms, and other content for toggling
+       <div class="collapse navbar-collapse" id="nav-atas">
+         <ul class="nav navbar-nav">
+           <li class="active"><a href="#">Page1</a></li>
+           <li><a href="#">Page2</a></li>
+         </ul>
+         <form class="navbar-form navbar-left" role="search">
+           <div class="form-group">
+             <input type="text" class="form-control" placeholder="Search">
+           </div>
+         </form>
+         <ul class="nav navbar-nav navbar-right">
+           <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+           <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+         </ul>
+       </div>/.navbar-collapse
+     </div>
+   </nav>  -->
+
+    <div class="container-fluid" id="main">
+      <!-- <nav>
+              <ul class="nav nav-pills">
+                <li><a href="#">Menu1</a></li>
+                <li><a href="#">Menu2</a></li>
+                <li><a href="#">Menu3</a></li>
+              </ul>
+            </nav> -->      
+
+      <div class="row">
+        <!-- yang kiri -->
+        <div class="col-xs-12 col-sm-3">
+          <div class="panel">
+            <div class="panel-body">
+              <div class="img">
+                <img src="img/1.jpg" alt="dedi" class="bulet">
+              </div>
+              
+              <div class="text-center">
+                <b>Dedi Arief Wibisono</b>
+                <div>Web Designer</div>
+                <div class="text-muted">"Do What You Love"</div>
+                <br>
+              </div>
+
+              <div class="row">
+                <div class="col-xs-6 col-xs-offset-3">
+                  <ul class="list-unstyled text-center">
+                    <li><a href="https://www.linkedin.com/in/dedi-wibisono-a06400100?trk=hp-identity-name" class="btn btn-primary btn-block">Visit me</a></li>
+                    <li><a href="https://www.dropbox.com/s/np412jag6waf8er/CV%20dedy.docx?dl=0" class="btn btn-default btn-block">Download CV</a></li>
+                    <li></li>
+                  </ul>
+                </div>
+              </div>
+
+              <div>
+              <!--   <div class="row">
+                <div class="col-xs-6 col-xs-offset-3">
+                  <span class="pull-left">
+                    <b>200</b>
+                    <p>test</p>
+                  </span>
+                  
+                  <span class="pull-right">
+                    <b>1.9M</b>
+                    <p>test</p>
+                  </span>
+                </div>
+              </div> -->
+
+                <div class="row">
+                  <div class="col-xs-12 text-center" style="box-shadow: 0 0 5px silver;">
+                  <label>My Contact</label>
+                    <ul class="list-unstyled">                 
+                    <li>dedi.wibisono17@gmail.com</li>
+                    <li>085737361067</li>
+                      <a href="https://www.facebook.com/dedyeinstein"><li>Facebook</li></a>
+                      <a href="https://twitter.com/dedyScofield"><li>Twitter</li></a>
+
+                    </ul>
+                  </div>
+
+                    <blackquote class="text-center">
+                            
+                    <p><i>Design is not just what it looks like and feels like. Design is how it works.</i></p>
+                    <footer>Steve Jobs</footer>
+                            
+                  </blackquote>
+
+                </div>
+
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- yang kiri -->
+
+        <!-- tengah -->
+        <div class="col-xs-12 col-sm-6">
+
+          <div class="panel">
+            <div class="panel-body">
+              <legend>
+                Work
+              </legend>
+              <div class="row">
+                <div class="col-xs-12 col-sm-3 col-md-4">
+                  <div class="thumbnail">
+                    <img src="img/popbox.jpg" alt="" class="img-responsive">
+                    <div class="caption">
+                      <h3>Event PopBox</h3>
+                      <a href="http://www.rakuten.co.id/event/popbox/"><i  class="text-center">Visit Page</i></a>
+                      <p>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-xs-12 col-sm-3 col-md-4">
+                  <div class="thumbnail">
+                    <img src="img/tripolis.jpg" alt="" class="img-responsive">
+                    <div class="caption">
+                      <h3>Newsletter</h3>
+                      <i>Send Email to Members</i>
+                      <p>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div class="col-xs-12 col-sm-3 col-md-4">
+                  <div class="thumbnail">
+                    <img src="img/banner.jpg" alt="" class="img-responsive">
+                    <div class="caption">
+                      <h3>Banner</h3>
+                      <i>Create Banner</i>
+                      <p>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="panel">
+            <div class="panel-body">
+              <legend>
+                Background
+              </legend>
+
+              <div class="bg-section">
+                <div class="bg-section-icon">
+                  <img src="img/employee-icon.jpg" alt="">
+                </div>
+                <div class="bg-section-content">
+                  <label>Summary</label>
+                   <ul class="list-unstyled">
+                     <li>2 Year Experience for HTML5, CSS3, and JavaScript</li>
+                     <li>2 Year Experience for Database MySQL</li>
+                     <li>2 Year Experience for Framework Design (Bootstrap)</li>
+                     <li>1 Year Experience for CodeIgniter</li>
+                     <li>1 Year Experience for Design Banner (Tools: Photoshop)</li>
+                   </ul>
+                </div>
+              </div>
+
+              <div class="bg-section">
+                <div class="bg-section-icon">
+                  <img src="img/coding.jpg" alt="">
+                </div>
+                <div class="bg-section-content">
+                  <label>Skills</label><br>
+                   
+                   <!-- skill -->
+                    <!-- 1 -->
+                   <div id="resumeProficiencies">
+    
+                   <div id="resumeProficienciesTop" class="resumeProficienciesTop1">HTML5 & CSS3</div><!--END resumeProficienciesTop-->
+
+                   <div id="resumeProficienciesBottom">
+                    <div class="progress">
+                      <div class="html progress-bar progress-bar-info" role="progressbar" style="width: 0%">95%
+                      </div>
+                    </div>
+                  </div>
+
+                  <!-- 2 -->
+                  <div id="resumeProficienciesTop" class="resumeProficienciesTop1">JavaScript</div><!--END resumeProficienciesTop-->
+
+                   <div id="resumeProficienciesBottom">
+                    <div class="progress">
+                      <div class="js progress-bar progress-bar-info" role="progressbar" style="width: 0%">85%
+                      </div>
+                    </div>
+                  </div>
+
+                  <!-- 3 -->
+                  <div id="resumeProficienciesTop" class="resumeProficienciesTop1">Bootstrap</div><!--END resumeProficienciesTop-->
+
+                   <div id="resumeProficienciesBottom">
+                    <div class="progress">
+                      <div class="bootstrap progress-bar progress-bar-info" role="progressbar" style="width: 0%">90%
+                      </div>
+                    </div>
+                  </div>
+
+                  <div id="resumeProficienciesTop" class="resumeProficienciesTop1">Photoshop</div><!--END resumeProficienciesTop-->
+
+                   <div id="resumeProficienciesBottom">
+                    <div class="progress">
+                      <div class="psd progress-bar progress-bar-info" role="progressbar" style="width: 0%">75%
+                      </div>
+                    </div>
+                  </div>
+                  </div> <!--END resumeProficienciesBottom-->
+
+                </div>
+              </div>
+              
+            </div>
+          </div>
+
+       
+        
+
+        </div>
+        <!-- tengah -->
+
+        <!-- kanan -->
+        <div class="col-xs-12 col-sm-3">
+        <!--   <label>People Also Viewed</label>
+          <br>
+        <div style="float: left;">
+          <img src="img/1.jpg" alt="" width="60">
+        </div>
+        <span><b>Nama Anda</b></span>
+        <div>haha</div>
+         -->
+
+           <div class="panel">
+            <div class="panel-body">
+              <legend>Recommendation</legend>
+              <div class="row">
+                <div class="col-xs-12">
+                  <blackquote class="blockquote-reverse">
+                            
+                    <p>Dedy has amazing inspiration as creative web design and also he has big desire to improve his skill. keep it and do more challenge ya!</p>
+                    <footer>Ahmad Rauf</footer>
+                            
+                  </blackquote>
+                </div>
+                            
+                <div class="col-xs-12">
+                  <blackquote class="blockquote-reverse">
+                            
+                    <p>Dedi is a very cool guy that he always tries anti-mainstream ways to achieve his tasks. He's funny, metal, and wierdo sometimes. But given the fact that he's much driven by a passion in UI/UX, as well as supported by his innovative insights, I beileve Dedi is a great asset for such Web Development works.</p>
+                    <footer>Jihad Dzikri Waspada</footer>
+                            
+                  </blackquote>
+                </div>
+                            
+                            
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- kanan -->
+
+      </div>
+    </div>
+
+
+
+
+
+
+
+    <footer>
+     <p style="text-align: center;">&copy Dedi Arief Wibisono 2016</p>
+
+
+   </footer>
+
+<script>
+  $(document).ready(function() {
+  $('.html').css('width', '95%');
+  $('.js').css('width', '85%');
+  $('.bootstrap').css('width', '90%');
+  $('.psd').css('width', '75%');
+  $('.jquery').css('width', '70%');
+  $('.javascript').css('width', '55%');
+  $('.bootstrap').css('width', '90%');
+  $('.wordpress').css('width', '85%');
+  $('.ui').css('width', '100%');
+  $('.ux').css('width', '90%');
+  $('.rwd').css('width', '85%');
+  $('.mobile').css('width', '85%');
+  $('.rapidproto').css('width', '95%');
+});
+</script>
+
+ </body>
+</html>
